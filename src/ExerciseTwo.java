30b79c8a4a5a57a8f39765c345c6579284d01292
Rafael Oliveira <rafaeloliver789@gmail.com>
2022-12-29 21:32:20 -0300

    Added exercise two

---
+++ src/ExerciseTwo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ExerciseTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int num = sc.nextInt();
+
+        System.out.println("O número informado é " + num);
+    }
+}

0f316e22e351daf4f7e53518926c4f1572fc7662
Rafael Oliveira <rafaeloliver789@gmail.com>
2022-12-29 22:10:50 -0300

    Added Exercise Three

---
+++ src/ExerciseThree.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ExerciseThree {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite os números: ");
+
+        int num = scan.nextInt();
+
+        int num1 = scan.nextInt();
+
+        System.out.println("Resultado: " + (num + num1));
+
+    }
+}

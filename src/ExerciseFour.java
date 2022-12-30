122f0e5fc182d14f45eb74e354b52fe956affcf3
Rafael Oliveira <rafaeloliver789@gmail.com>
2022-12-29 22:30:15 -0300

    Added Exercise Four

---
+++ src/ExerciseFour.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ExerciseFour {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite os numeros: ");
+
+        float bim1 = sc.nextFloat();
+        float bim2 = sc.nextFloat();
+        float bim3 = sc.nextFloat();
+        float bim4 = sc.nextFloat();
+
+        System.out.printf("A média do Bimestre é %.1f", (bim1 + bim2 + bim3 + bim4) / 4 );
+    }
+}

c8780f0be30e79bbf5545a21fe4291b8c522ee5b
Rafael Oliveira <rafaeloliver789@gmail.com>
2023-02-24 21:41:34 -0300

    Added Exercise Thirty Six

--- src/matriz/ExerciseThirtyFour.java
+++ src/matriz/ExerciseThirtyFour.java
@@ -1,7 +1,10 @@
 package matriz;
 
 import java.util.Scanner;
-
+//Faça um programa para armazenar em uma matriz os compromissos de uma agenda pessoal. Cada dia do mês deve conter 24h
+//onde para cada uma destas 24h podemos associar uma tarefa específica (compromisso agendado). O programa deve ter um
+//menu  onde o usuario indica o dia do mês que deseja alterar e a hora, entrando em seguida com o compromisso, ou então,
+//o usuário pode também consultar a agenda, fornecendo o dia e a hora para obter o compromisso armazenado.
 public class ExerciseThirtyFour {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
